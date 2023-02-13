815964f286ea814bbc086d1806e148097e546fbe
Valentina-K <kvalentyna@hotmail.com>
2023-02-13 11:51:55 +0200

    changed some path in import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { Home } from '../pages/Home';
-import { Movies } from '../pages/Movies';
+import { Home } from 'pages/Home';
+import { Movies } from 'pages/Movies';
 import { MoviesDetails } from 'pages/MovieDetails';
 import { Cast } from './Cast/Cast';
 import { Reviews } from './Reviews/Reviews';
@@ -30,14 +30,3 @@ export const App = () => {
     </Container>
   );
 };
-/* '/' - компонент Home, домашняя страница со списком популярных кинофильмов.
-'/movies' - компонент Movies, страница поиска фильмов по ключевому слову.
-'/movies/:movieId' - компонент MovieDetails, страница с детальной информацией о кинофильме.
-/movies/:movieId/cast - компонент Cast, информация о актерском составе. Рендерится на странице MovieDetails.
-/movies/:movieId/reviews - компонент Reviews, информация об обзорах. Рендерится на странице MovieDetails. */
-
-/* /trending/get-trending список самых популярных фильмов на сегодня для создания коллекции на главной странице.
-/search/search-movies поиск кинофильма по ключевому слову на странице фильмов.
-/movies/get-movie-details запрос полной информации о фильме для страницы кинофильма.
-/movies/get-movie-credits запрос информации о актёрском составе для страницы кинофильма.
-/movies/get-movie-reviews запрос обзоров для страницы кинофильма. */
